e9eb17bbc3ccdcfbdce449274b3ed699a08e03c5
abhishek <abhishekmore710@gmail.com>
2020-12-18 23:56:44 +0530

    done like auslander

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -8,27 +8,8 @@ function HeroSection() {
 		<div className="hero-container">
 			<video src="/videos/video-2.mp4" autoPlay loop muted />
 			<h1>PARALLEL-O-CODE</h1>
-			<p>Shreyash Mandade<br></br>Aditya Gadadhani<br></br>Abhishek More </p>
-			<div className="hero-btns">
-				<Button className="btns" buttonStyle="btn--outline" buttonSize="btn--large" ok="same">
-					GET STARTED
-				</Button>
-				<Button
-					className="btns"
-					buttonStyle="btn--primary"
-					buttonSize="btn--large"
-					ok="youtube">
-					<a
-						style={{ color: "inherit", textDecoration: "none" }}
-						href="https://www.youtube.com/playlist?list=PLTSNssSyrMjmMVTfBs2fsKbzqMR5cynee"
-						target="_blank"
-						rel="noopener noreferrer">
-						{" "}
-						WATCH TRAILER{" "}
-					</a>{" "}
-					<i className="far fa-play-circle"></i>
-				</Button>
-			</div>
+			<p>Miss.P.Mundada<br></br>Shreyash Mandade<br></br>Aditya Gadadhani<br></br>Abhishek More </p>
+		
 		</div>
 	);
 }
